a14189467c06832334a9b90c4abc00a151cffbbe
gmajors1326 <gmajors1326@gmail.com>
2026-06-04 07:54:47 -0500

    cleanup: remove redundant backup zip archives, unused images in lite theme, and bump plugin version

--- varner-os-plugin-v23-unpacked/varner-os-plugin-v23/varner-os-plugin-v23.php
+++ varner-os-plugin-v23-unpacked/varner-os-plugin-v23/varner-os-plugin-v23.php
@@ -1,8 +1,8 @@
 <?php
 /**
  * Plugin Name: Varner OS Plugin v23
- * Description: Version 1.23.9 - React-powered inventory management for Varner Equipment.
- * Version: 1.23.9
+ * Description: Version 1.23.10 - React-powered inventory management for Varner Equipment.
+ * Version: 1.23.10
  * Author: hwy559.com
  */
 
